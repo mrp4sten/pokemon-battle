6e1990767a37733bbeb2dc781db55ad0a8d3a74f
mrp4sten <mauricio.pasten.martinez@gmail.com>
2024-03-02 01:34:58 -0600

    Add code to check if file exists before loading data

--- load_150_pokemons.py
+++ load_150_pokemons.py
@@ -1,31 +1,33 @@
 import requests
 import json
+import os
 
 response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=150")
 
 pokemons_with_data = []
 
-if response.status_code == 200:
-    jsonData = response.json()
-    pokemons_names = jsonData["results"]
+if not os.path.exists('pokemons_full_data.json'):
+    if response.status_code == 200:
+        jsonData = response.json()
+        pokemons_names = jsonData["results"]
 
-    for pokemon in pokemons_names:
-        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon['name']}")
+        for pokemon in pokemons_names:
+            response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon['name']}")
 
-        if response.status_code == 200:
-            pokemon_data = response.json()
-            pokemons_with_data.append(pokemon_data)
-        elif response.status_code >= 400 and response.status_code < 500:
-            print(f"Client error, failed to get data for {pokemon['name']}")
-        elif response.status_code >= 500:
-            print(f"Server error, failed to get data for {pokemon['name']}")
-        else:
-            print(f"Failed to get data for {pokemon['name']}")
+            if response.status_code == 200:
+                pokemon_data = response.json()
+                pokemons_with_data.append(pokemon_data)
+            elif response.status_code >= 400 and response.status_code < 500:
+                print(f"Client error, failed to get data for {pokemon['name']}")
+            elif response.status_code >= 500:
+                print(f"Server error, failed to get data for {pokemon['name']}")
+            else:
+                print(f"Failed to get data for {pokemon['name']}")
 
-elif response.status_code >= 404 and response.status_code < 500:
-    print("Client error")
-elif response.status_code >= 500:
-    print("Internal server error")
+    elif response.status_code >= 404 and response.status_code < 500:
+        print("Client error")
+    elif response.status_code >= 500:
+        print("Internal server error")
 
-with open("pokemons_full_data.json", "w") as file:
-    json.dump(pokemons_with_data, file, indent=4)
+    with open("pokemons_full_data.json", "w") as file:
+        json.dump(pokemons_with_data, file, indent=4)
--- pokemon_catalog.py
+++ pokemon_catalog.py
@@ -1,31 +1,34 @@
 import json
+import os
 
-with open("pokemons_full_data.json", "r") as f:
-    pokemons = json.load(f)
+if not os.path.exists('pokemon_detils.json'):
+    with open("pokemons_full_data.json", "r") as f:
+        pokemons = json.load(f)
 
-pokemon_details = []
-for pokemon in pokemons:
-    basic_attack = (
-        pokemon["moves"][0]["move"]["name"] if len(pokemon["moves"]) > 0 else "Unknown"
-    )
-    hard_attack = (
-        pokemon["moves"][1]["move"]["name"] if len(pokemon["moves"]) > 1 else "Unknown"
-    )
-    pokemon_details.append(
-        {
-            "name": pokemon["name"],
-            "experience": pokemon["base_experience"],
-            "health": 100,
-            "type": pokemon["types"][0]["type"]["name"],
-            "basic_attack": {"name": basic_attack, "damage": 30},
-            "hard_attack": {"name": hard_attack, "damage": 40},
-            "ability": {
-                "name": pokemon["abilities"][0]["ability"]["name"],
-                "damage": 50,
-            },
-            "block": {"damage": 20},
-        }
-    )
 
-with open("pokemon_detils.json", "w") as file:
-    json.dump(pokemon_details, file, indent=4)
+    pokemon_details = []
+    for pokemon in pokemons:
+        basic_attack = (
+            pokemon["moves"][0]["move"]["name"] if len(pokemon["moves"]) > 0 else "Unknown"
+        )
+        hard_attack = (
+            pokemon["moves"][1]["move"]["name"] if len(pokemon["moves"]) > 1 else "Unknown"
+        )
+        pokemon_details.append(
+            {
+                "name": pokemon["name"],
+                "experience": pokemon["base_experience"],
+                "health": 100,
+                "type": pokemon["types"][0]["type"]["name"],
+                "basic_attack": {"name": basic_attack, "damage": 30},
+                "hard_attack": {"name": hard_attack, "damage": 40},
+                "ability": {
+                    "name": pokemon["abilities"][0]["ability"]["name"],
+                    "damage": 50,
+                },
+                "block": {"damage": 20},
+            }
+        )
+
+    with open("pokemon_detils.json", "w") as file:
+        json.dump(pokemon_details, file, indent=4)
